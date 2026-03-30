aa194ec91653fa10dae3c313f599e064e09dc237
Mark Novak <mnovak.m@gmail.com>
2026-03-30 11:06:54 -0700

    Randomize names

--- admin/R/RandomizeNames.R
+++ admin/R/RandomizeNames.R
@@ -1,21 +1,18 @@
 library(readxl)
 
-dat<-read_xls(file.choose(), 
-              skip = 14)
+dat <- read_xls(file.choose(), skip = 14)
 
 rand <- sample(dat$'Student Name')
-write.csv(rand,
-          'RandomNames.csv',
-          row.names=FALSE)
+write.csv(rand, 'RandomNames.csv', row.names = FALSE)
 
 
 # Pair-up accountabilibuddies
-pairOff <- function(x){
-  pairs <- matrix(sample(x),ncol=2)
+pairOff <- function(x) {
+  pairs <- matrix(sample(x), ncol = 2)
   return(pairs)
 }
-buddies <- pairOff(rand)
 
+buddies <- pairOff(rand)
 write.csv(buddies,
           'Accountabilibuddies.csv',
-          row.names=FALSE)
+          row.names = FALSE)
